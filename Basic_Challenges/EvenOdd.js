e269334b9875444dc1867c511be5bac9d537d6da
Robison Torres <robison.torres21@gmail.com>
2022-08-15 15:16:14 -0300

    Initial commit.

---
+++ Basic_Challenges/EvenOdd.js
@@ -0,0 +1,10 @@
+console.log("From Code Wars")
+console.log()
+
+function even_or_odd(number) {
+    
+    // This function determines if a number is ever or odd.
+    return number % 2 ? "Even": "Odd";}
+
+console.log(even_or_odd(5))
+
