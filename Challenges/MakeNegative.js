336b64f8758d58b153bcab679f1a74113adcb1ce
Robison Torres <robison.torres21@gmail.com>
2022-08-17 11:25:34 -0300

    Initial commit.

---
+++ Challenges/MakeNegative.js
@@ -0,0 +1,10 @@
+console.log("From Code Wars")
+console.log()
+
+function makeNegative(num) {
+
+    // This fuction return any given number to its negative form.
+    return (num > 0) ? num * -1: num;
+}
+
+console.log(makeNegative(5))
