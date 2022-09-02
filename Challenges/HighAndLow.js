eec2be6fb04320227e5e6538d8c7c3758ff751e2
Robison Torres <robison.torres21@gmail.com>
2022-09-02 09:15:52 -0300

    Initial commit.

---
+++ Challenges/HighAndLow.js
@@ -0,0 +1,14 @@
+console.log("From Code Wars")
+console.log()
+
+function highAndLow(numbers){
+    
+    /* This function takes a string of numbers and returns the max 
+    and min numbers separeted by space.*/
+    let nums = numbers.split(" ").map(num => parseInt(num))
+    return String(Math.max(...nums)) + " " + String(Math.min(...nums))
+    /* "…" in front of an array will convert array to distinct
+    variables and send them to the function.*/
+}
+
+console.log(highAndLow("1 2 3 4 5"))
