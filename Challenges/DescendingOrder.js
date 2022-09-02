af5168d494f422178b79ff2055171465781f10e5
Robison Torres <robison.torres21@gmail.com>
2022-09-02 11:55:53 -0300

    Initial commit.

---
+++ Challenges/DescendingOrder.js
@@ -0,0 +1,13 @@
+console.log("From Code Wars")
+console.log()
+
+function descendingOrder(n){
+    
+    /* This function takes any given number and returns 
+    its digits in descend order.*/
+    return parseInt(String(n).split('')
+    .sort((a,b)=>b-a) // This operation reverses the order.
+    .join(''))   
+}
+
+console.log(descendingOrder(586915)) // Outputs - 986551
--- Challenges/test_free.js
+++ Challenges/test_free.js
@@ -1,13 +1,3 @@
 console.log("Test Free - keep it clean.")
 console.log()
 
-function descendingOrder(n){
-    
-    /* This function takes any given number and returns 
-    its digits in descend order.*/
-    return parseInt(String(n).split('')
-    .sort((a,b)=>b-a) // This operation reverses the order.
-    .join(''))   
-}
-
-console.log(descendingOrder(586915)) // Outputs - 986551
