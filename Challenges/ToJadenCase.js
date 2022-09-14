d2b0b5d18da5b651b0aa509e9a4a06d81cd9cdeb
Robison Torres <robison.torres21@gmail.com>
2022-09-14 10:38:28 -0300

    Initial commit.

--- Challenges/ToJadenCase.js
+++ Challenges/ToJadenCase.js
@@ -5,12 +5,18 @@ String.prototype.toJadenCase = function() {
 
     // This function capitalizes the first letter of each word.
     const new_list = [];
+
+    // In JavaScript, the "this" keyword refers to an object.
     text = this.split(" ")
     for (let i = 0; i < text.length; i++) {
         new_list.push(text[i].slice(0,1).toUpperCase()
         + text[i].slice(1));
     }
     return new_list.join(" ");
+
+    /* return this.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' '); - Clever. */
 };
 
 var text = "How can mirrors be real if our eyes aren't real."
