41b093acea49d3890759d0552f148e7a0e3496bf
Robison Torres <robison.torres21@gmail.com>
2022-10-19 10:51:40 -0300

    Initial commit.

---
+++ Challenges/OpenOrSenior.js
@@ -0,0 +1,8 @@
+console.log("From Code Wars")
+console.log()
+
+function openOrSenior(data){
+
+    /* This function categorizes a person as 'Senior' or 'Open'
+    based on his/ her age and 'handicap'.*/
+}
--- Challenges/test_free.js
+++ Challenges/test_free.js
@@ -1,3 +1,5 @@
 console.log("Test Free - keep it clean.")
 console.log()
 
+club = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+console.log(club[0][0])
