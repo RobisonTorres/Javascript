2948d7c1136ae7e66a1c5741d1195802a0e8e99a
Robison Torres <robison.torres21@gmail.com>
2022-10-20 10:47:03 -0300

    Initial commit.

--- Challenges/OpenOrSenior.js
+++ Challenges/OpenOrSenior.js
@@ -5,4 +5,19 @@ function openOrSenior(data){
 
     /* This function categorizes a person as 'Senior' or 'Open'
     based on his/ her age and 'handicap'.*/
+    let result = [];
+    var x = 0;
+    while (x < data.length) {
+        if (data[x][0] >= 55 && data[x][1] > 7){
+            result.push('Senior')
+            x += 1
+        } else {
+            result.push('Open')
+            x += 1
+        };
+    }
+    return result
 }
+
+console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6],
+                         [21, 21], [78, 9]]))
--- Challenges/test_free.js
+++ Challenges/test_free.js
@@ -1,5 +1,3 @@
 console.log("Test Free - keep it clean.")
 console.log()
 
-club = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
-console.log(club[0][0])
