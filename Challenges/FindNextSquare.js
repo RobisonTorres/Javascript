1fa2030877c62460850719cede9cce57469e6776
Robison Torres <robison.torres21@gmail.com>
2022-10-21 10:49:02 -0300

    Initial commit.

---
+++ Challenges/FindNextSquare.js
@@ -0,0 +1,8 @@
+console.log("From Code Wars")
+console.log()
+
+function findNextSquare(sq) {
+
+    // Return the next square if sq is a perfect square, -1 otherwise
+    return -1;
+}
--- Challenges/test_free.js
+++ Challenges/test_free.js
@@ -3,6 +3,17 @@ console.log()
 
 function findNextSquare(sq) {
 
-    // Return the next square if sq is a perfect square, -1 otherwise
-    return -1;
+    var test = Math.sqrt(sq)
+    if (Number.isInteger(test)){
+        let x = test+1
+        let y = Number.isInteger(x**x)
+        while (y == false){
+            x += 1};
+        return x*x;
+    } else {
+        return -1;
+    }
 }
+
+console.log(findNextSquare(100))
+console.log(Number.isInteger(Math.sqrt(4**4)))
