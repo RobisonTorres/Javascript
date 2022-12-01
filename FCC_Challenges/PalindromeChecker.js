8e60229199cfb5139688fa3723fe4d49da58232c
Robison Torres <robison.torres21@gmail.com>
2022-12-01 11:18:50 -0300

    Initial Commit.

---
+++ FCC_Challenges/PalindromeChecker.js
@@ -0,0 +1,15 @@
+console.log("From Free Code Camp.")
+console.log()
+
+function palindrome(str) {
+
+    /* Given a string, this function returns 'true' for a
+    palindrome and 'false' otherwise.*/
+    str = str.toLowerCase().match(/[a-zA-Z0-9]/gi)
+    return str.join('') == str.reverse().join('')
+}
+  
+console.log(palindrome("1 eye for of eye 1."))  // Outputs - true.
+
+/* A palindrome is a word or sentence that's spelled the same way 
+both forward and backward, ignoring punctuation, case, and spacing. */
---
+++ FCC_Challenges/RomanNumeralConverter.js
@@ -0,0 +1,10 @@
+console.log("From Free Code Camp.")
+console.log()
+
+function convertToRoman(num) {
+
+    // This function converts the given number into a roman numeral.
+   return 'XLIV';
+}
+  
+console.log(convertToRoman(36))
