1577c9c5f9c2c0f6b67813cb5072d44282c65bbf
Robison Torres <robison.torres21@gmail.com>
2022-12-02 11:10:26 -0300

    Initial commit.

--- FreeCodeProjects/RomanNumeralConverter.js
+++ FreeCodeProjects/RomanNumeralConverter.js
@@ -3,8 +3,28 @@ console.log()
 
 function convertToRoman(num) {
 
-    // This function converts the given number into a roman numeral.
-   return 'XLIV';
+    /* This function takes a positive integer (1 - 3999) as its parameter and returns
+    a string containing the Roman Numeral representation of that integer.*/
+    var roman_numbers = {
+        1000: 'M', 2000: 'MM', 3000: 'MMM',  
+        100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC',
+        700: 'DCC', 800: 'DCCC', 900: 'CM', 
+        10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX',
+        70: 'LXX', 80: 'LXXX', 90: 'XC', 
+        1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI',
+        7: 'VII', 8: 'VIII', 9: 'IX'
+    }
+    let n = String(num).split('')
+    let r_num = ''
+    for (let i = 0; i < n.length; i++){
+        let a = n.slice(i+1, n.length)
+        if (n[i] == '0') {
+            continue;
+        } else {
+            r_num += roman_numbers[n[i] + '0'.repeat(a.length)]
+        }
+    }
+    return r_num
 }
   
-console.log(convertToRoman(36))
+console.log(convertToRoman(3306))
