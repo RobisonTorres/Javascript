3e0987a7627b68ae71e051d20da1d11602e5bdcb
Robison Torres <robison.torres21@gmail.com>
2022-12-07 17:01:38 -0300

    Initial commit.

--- BasicChallenges_7kyu/test_free.js
+++
@@ -1,3 +0,0 @@
-console.log("Test Free - keep it clean.")
-console.log() // To start/restart Quokka press Ctrl K, Q.
-
--- FreeCodeProjects/CashRegister.js
+++ FreeCodeProjects/CashRegister.js
@@ -2,17 +2,22 @@ console.log("JavaScript Algorithms and Data Structures - Projects.")
 console.log()
 
 function checkCashRegister(price, cash, cid) {
-    let change;
+
+    // This function...
+    let change = [];
+    let x = 8;  // Current units
+    let change_cash = (cash - price).toFixed(2);
+    while (x >= 0) {
+        if (){  // TODO - Condition
+            change.push([cid[x][0], (cid[x][1] % change_cash).toFixed(2)]);
+            change_cash = (change_cash % cid[x][1]).toFixed(2);
+        }
+        x--;
+    }
     return change;
 }
-  
+
 console.log(checkCashRegister(19.5, 20, 
-            [["PENNY", 1.01], 
-            ["NICKEL", 2.05], 
-            ["DIME", 3.1], 
-            ["QUARTER", 4.25], 
-            ["ONE", 90], 
-            ["FIVE", 55], 
-            ["TEN", 20], 
-            ["TWENTY", 60], 
-            ["ONE HUNDRED", 100]]))
+                    [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], 
+                    ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], 
+                    ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
