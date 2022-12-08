09f04f580320b21c83b98b5026f788936194715c
Robison Torres <robison.torres21@gmail.com>
2022-12-08 10:33:00 -0300

    Initial commit.

--- FreeCodeProjects/CashRegister.js
+++ FreeCodeProjects/CashRegister.js
@@ -3,31 +3,49 @@ console.log()
 
 function checkCashRegister(price, cash, cid) {
 
-    // This function...
+    /* Task - Design a cash register drawer function checkCashRegister() 
+    that accepts purchase price as the first argument (price), payment as 
+    the second argument (cash), and cash-in-drawer (cid) as the third argument.*/
     let change = [];
     let x = 8;  // current units
-    let values = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100]
+    let values = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];  // current values
     let change_cash = (cash - price).toFixed(2);
     
     while (x >= 0 && change_cash >= 0) {
 
         if (change_cash % cid[x][1] < change_cash){
-            change.push(cid[x][0], cid[x][1])
-            change_cash = (change_cash - cid[x][1]).toFixed(2)
+            change.push([cid[x][0], cid[x][1]]);
+            change_cash = (change_cash - cid[x][1]).toFixed(2);
             
         } else if (change_cash % cid[x][1] == change_cash && (change_cash/ values[x]) >= 1 ){
-            change.push(cid[x][0], parseInt(change_cash / values[x]) * values[x])
-            change_cash = (change_cash - parseInt(change_cash / values[x]) * values[x]).toFixed(2)
+            change.push([cid[x][0], parseInt(change_cash / values[x]) * values[x]]);
+            change_cash = (change_cash - parseInt(change_cash / values[x]) * values[x]).toFixed(2);
         }
         x--;
     }
-    return change;
+    let cid_sum = cid.reduce((p, c) => p + c[1], 0);
+    let change_sum = change.reduce((p, c) => p + c[1], 0);
+
+    if (cid_sum > change_sum && change_cash == 0) {
+        return {status: "OPEN", change: change};
+    } else if (cid_sum == change_sum && change_cash == 0) {
+        return {status: "CLOSED", change: cid};
+    } else {
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
 }
 
-console.log(checkCashRegister(19.50, 20, 
-    [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+console.log(checkCashRegister(3.26, 100, 
+                            [["PENNY", 1.01], ["NICKEL", 2.05], 
+                            ["DIME", 3.1], ["QUARTER", 4.25], 
+                            ["ONE", 90], ["FIVE", 55], ["TEN", 20], 
+                            ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 
-/* Outputs - ['TWENTY',  60, 'TEN', 20, 'FIVE', 15, 
-              'ONE', 1, 'QUARTER', 0.5, 'DIME', 0.2,
-              'PENNY',   0.04
-]*/
+/* Outputs - {
+
+    status: 'OPEN', change: [
+    [ 'TWENTY', 60 ], [ 'TEN', 20 ], [ 'FIVE', 15 ], 
+    [ 'ONE', 1 ], [ 'QUARTER', 0.5 ], [ 'DIME', 0.2 ],
+    [ 'PENNY', 0.04 ]]
+
+}*/                    
