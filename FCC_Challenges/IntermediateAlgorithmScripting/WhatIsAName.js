b2aaed9f23c1050f2bd2b3eab44a666544984a27
Robison Torres <robison.torres21@gmail.com>
2022-12-14 18:25:40 -0300

    function done.

--- FCC_Challenges/IntermediateAlgorithmScripting/WhatIsAName.js
+++ FCC_Challenges/IntermediateAlgorithmScripting/WhatIsAName.js
@@ -3,18 +3,24 @@ console.log()
 
 function whatIsInAName(collection, source) {
 
-    // This function...
+    /* Task - Make a function that looks through an array of objects 
+    (first argument) and returns an array of all objects that have matching
+    name and value pairs (second argument)*/
     let newArr = []
-    let keys_collections = []
-    for (let i = 0; i < collection.length; i++){
-        newArr.push(collection[i]);
-        keys_collections.push(Object.keys(collection[i]))
+    let keys_col = Object.keys(source)  // ['apple', 'bat']
+    for (let i = 0; i < collection.length; i++){ 
+        if ((keys_col.every(key => collection[i].hasOwnProperty(key))) &
+            keys_col.every(key => collection[i][key] == source[key])) {
+            newArr.push(collection[i])
+        }
     }
-    return new Set(keys_collections.flat(1))
+   return newArr
 }
 
 console.log(whatIsInAName(
-            [{ first: "Romeo", last: "Montague" }, 
-            { first: "Mercutio", last: 'null' }, 
-            { first: "Tybalt", last: "Capulet" }], 
-            { first: "Tybalt", last: "Capulet"}));
+    [{ "apple": 1, "bat": 2 }, 
+    { "bat": 2 }, 
+    { "apple": 1, "bat": 2, "cookie": 2 }], 
+    { "apple": 1, "bat": 2 }))
+
+// Outputs - [ { apple: 1, bat: 2 }, { apple: 1, bat: 2, cookie: 2 } ]
