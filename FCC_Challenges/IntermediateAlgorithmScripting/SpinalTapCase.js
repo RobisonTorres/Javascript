849de9b484ad7895d4a9ab7246f6f2e57231d723
Robison Torres <robison.torres21@gmail.com>
2022-12-15 10:34:17 -0300

    Function done.

---
+++ FCC_Challenges/IntermediateAlgorithmScripting/SpinalTapCase.js
@@ -0,0 +1,14 @@
+console.log("From Free Code Camp")
+console.log()
+
+function spinalCase(str) {
+    
+    /* Task - Convert a string to spinal case. 
+    Spinal case is all-lowercase-words-joined-by-dashes. */
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2')  // separate lower characters from upper ones.
+    str = str.replace(/[\W_/]+/g, '-')  // replace all special characters to "-".
+    str = str.toLowerCase()
+   return str;
+}
+
+console.log(spinalCase("AllThe-small Things"))  // Outputs - all-the-small-things.
