0ae8cd29352172a38ca8fd1faa283c55d939eec2
RobisonTorres <robison.torres21@gmail.com>
2023-12-29 14:45:07 -0300

    first commit.

---
+++ BasicChallenges_7kyu/endsWith.js
@@ -0,0 +1,10 @@
+console.log("From Code Wars")
+console.log()
+
+function solution(str, ending){
+
+    // This function returns true if a str ends with ending.
+    return str.endsWith(ending);
+}
+
+console.log(solution("Cats are the best!", "best!"))
